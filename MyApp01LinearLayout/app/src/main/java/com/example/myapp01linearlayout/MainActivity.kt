d162bc818d2ff828ae31becd61c2612b546c8220
Lucie Strnadkova <lucie.strnadkova1@tul.cz>
2024-09-26 15:21:07 +0200

    finished app

--- MyApp01LinearLayout/app/src/main/java/com/example/myapp01linearlayout/MainActivity.kt
+++ MyApp01LinearLayout/app/src/main/java/com/example/myapp01linearlayout/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapp01linearlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+           } */
+
+       val etName = findViewById<EditText>(R.id.etName) //reference na EditText
+       val etLastname = findViewById<EditText>(R.id.etLastname)
+       val etPlace = findViewById<EditText>(R.id.etPlace)
+       val etAge = findViewById<EditText>(R.id.etAge)
+       val tvInformation = findViewById<TextView>(R.id.tvInformation)
+       val btnSend = findViewById<Button>(R.id.btnSend)
+       val btnDelete = findViewById<Button>(R.id.btnDelete)
+
+        //nastaveni obsluhy pro tlacitka
+
+        btnSend.setOnClickListener{
+            val name = etName.text.toString()
+            val surname = etLastname.text.toString()
+            val place = etPlace.text.toString()
+            val age = etAge.text.toString() //muzeme dal prevest na int
+
+            // zobrazeni textu v TextView
+            val formattedText = "Jmenuji se $name $surname. Je mi $age let a moje bydliště je $place."
+            tvInformation.text = formattedText
+        }
+
+        btnDelete.setOnClickListener{
+            etName.text.clear()
+            etLastname.text.clear()
+            etPlace.text.clear()
+            etAge.text.clear()
+
+            tvInformation.text = ""
         }
     }
 }
