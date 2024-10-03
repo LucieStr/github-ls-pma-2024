7692c00973c674e95425b8932e86ff98431720dd
Lucie Strnadkova <lucie.strnadkova1@tul.cz>
2024-10-03 15:25:49 +0200

    #3 souhrn objednavky

--- MyApp03Objednavka/app/src/main/java/com/example/myapp03objednavka/MainActivity.kt
+++ MyApp03Objednavka/app/src/main/java/com/example/myapp03objednavka/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.myapp03objednavka
 
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp03objednavka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-//       ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-    //           insets
-       // }
+
+        //binding setting
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = "Objednávka prstenu"
+
+        //nastaveni tlacitka
+        binding.btnObjednavka.setOnClickListener{
+            val prstenRbId = binding.rPrsten.checkedRadioButtonId
+
+            //to co se zaskrtne se da do val prsten
+            val prsten = findViewById<RadioButton>(prstenRbId)
+
+            val krouzky = binding.chKrouzky.isChecked
+            val barva = binding.chBarvaK.isChecked
+            val jeden = binding.chJedenK.isChecked
+
+            val objednavkaText = "Souhrn objednávky: " +
+                    "${prsten.text}" + " prsten" +
+                    (if(krouzky) "; méně kroužků" else "") +
+                    (if(barva) "; zelená barva kamenů" else "") +
+                    (if(jeden) "; prsten bude mít jen jeden kámen ve vybrané barvě" else "")
+
+            binding.tvObjednavka.text = objednavkaText
+        }
+
     }
 }
