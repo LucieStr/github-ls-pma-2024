913f4b758ab5b501cdf9c2b8f94b0b59a4589aff
Lucie Strnadkova <lucie.strnadkova1@tul.cz>
2024-10-10 17:43:40 +0200

    5 first done

---
+++ MyApp05MoreActivities/app/src/main/java/com/example/myapp05moreactivities/SecondActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapp05moreactivities
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+        val twInfo = findViewById<TextView>(R.id.twInfo)
+
+        // Načtení dat z intentu
+        val nickname = intent.getStringExtra("NICK_NAME")
+        twInfo.text = "Data z první aktivity. Jméno: $nickname"
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+    }
+}
