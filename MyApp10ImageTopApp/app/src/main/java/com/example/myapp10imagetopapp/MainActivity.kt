842ebcdcbfab45db1ef2c290f03c3d4636832ced
Lucie Strnadkova <lucie.strnadkova1@tul.cz>
2024-11-07 11:15:05 +0100

    #9 get picture

--- MyApp10ImageTopApp/app/src/main/java/com/example/myapp10imagetopapp/MainActivity.kt
+++ MyApp10ImageTopApp/app/src/main/java/com/example/myapp10imagetopapp/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.myapp10imagetopapp
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp10imagetopapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        // inicializace pro viewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            uri: Uri? -> binding.ivImage.setImageURI(uri) //definuje, co se ma provest po dokonceni akce
+        }
+
+        //co ziskame po kliknuti na tlacitko 
+        binding.btnTakeImage.setOnClickListener{
+            getContent.launch("image/*")
+
         }
     }
 }
